aafbc9e77440b3ec70bca65b76d4590af56932a6
Dave Becker <dmb43016@gmail.com>
2020-02-28 17:15:57 -0500

    Stuff

--- Visualizer/Visualizer.ino
+++ Visualizer/Visualizer.ino
@@ -6,11 +6,11 @@
 #include "arduinoFFT.h"
 
 #define NUM_LEDS 60
-#define LED_PIN 5
+#define LED_PIN 5 // D1 on ESP8266
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 
-#define MIC_PIN 0 // A0
+#define MIC_PIN 0 // A0 on ESP8266
 
 // Modes
 #define OFF 1
@@ -29,14 +29,15 @@
 
 // Sampling
 #define SAMPLES 256             //Must be a power of 2
-#define SAMPLING_FREQUENCY 10000 //Hz, must be less than 10000 due to ADC
+#define SAMPLING_FREQUENCY 9000 //Hz, must be less than 10000 due to ADC, Max frequency able to be detected is half this value
 const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
 unsigned int sample;
-arduinoFFT FFT = arduinoFFT();
-double vReal[SAMPLES];
-double vImag[SAMPLES];
+double vReal[SAMPLES]; // Holds real parts of FFT bins
+double vImag[SAMPLES]; // Holds imaginary parts of FFT bins
 unsigned int sampling_period_us;
 unsigned long microseconds;
+
+arduinoFFT FFT = arduinoFFT();
 
 // LED strip
 CRGB leds[NUM_LEDS];
@@ -137,11 +138,12 @@ void musicReactive() {
 
 // Music mode -> volume
 void volume() {
+  // Get peak to peak volts from mic during sample window
   double volts = readMic();
   if (volts == -1) {
     return;
   }
-  Serial.println(volts);
+  
   double pixelNum = (NUM_LEDS / 4.95) * volts; // 4.95 is max volts, so normalize to NUM_LEDS
 
   // Cap at 60 just in case
@@ -164,51 +166,60 @@ void volume() {
 
 // Music mode -> freq_1
 void freq_1() {
-  for (int i = 0; i < SAMPLES; i++)
-  {
-    microseconds = micros();    //Overflows after around 70 minutes!
+  
+  fft_sample();
 
-    vReal[i] = analogRead(0);
-    vImag[i] = 0;
-
-    while (micros() < (microseconds + sampling_period_us)) {
-    }
-  }
-
-  /*FFT*/
+  // FFT
   FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
   FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
   FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
 
+  // Count is uesd to check if a given frequency was detected
+  // If it wasn't, segment needs to be cleared
   int countB = 0;
   int countM = 0;
   int countH = 0;
-    
-  for (int i = 2; i < (SAMPLES / 2); i++)
-  {
+  
+  // Loop through bins
+  for (int i = 2; i < (SAMPLES / 2); i++) {
     yield();
     if(mode != MUSIC && musicMode != FREQ_1) return;
+
+    // Frequency is used to make code easier to understand
     double frequency = (i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES;
     
+    // Filter out noise (lower magnitudes)
     if(vReal[i] > 500){
+
+      // Bass frequencies
       if(frequency > 50 && frequency < 80){
         countB++;
+        // 2000 is an arbitrary number
+        // TODO: Change number
         double val = vReal[i] / 2000.0;
+        // Set band 0 to the percentage * LED number
         freq_1_Band(0, (int)(val * 30));
+      // Mid frequencies
       } else if(frequency > 150 && frequency < 2000){
         // TODO: Find peak vReal in frequency range and only display band on that one
         countM++;
+        // 2000 is an arbitrary number
+        // TODO: Change number
         double val = vReal[i] / 2000.0;
+        // Set band 1 to the percentage * LED number
         freq_1_Band(1, (int)(val * 27));
-      }
-      else if(frequency > 2000){
+      // High frequencies
+      } else if(frequency > 2000){
+        // Remove more noise
         if(vReal[i] > 15000){
           countH++;
+          // Set band 2 to have all 3 LEDS lit
           freq_1_Band(2, 3);
         }
       }
     }
   }
+  // Check if any frequencies weren't detected and clear the bands
   if(countB == 0){
     for(int i = 3; i < 30; i++){
       leds[i] = CRGB::Black;
@@ -220,14 +231,17 @@ void freq_1() {
     }
   }
   if(countH == 0){
+    // Bottom LEDs
     leds[0] = CRGB::Black;
     leds[1] = CRGB::Black;
     leds[2] = CRGB::Black;
     
+    // Top LEDs
     leds[NUM_LEDS - 1] = CRGB::Black;
     leds[NUM_LEDS - 2] = CRGB::Black;
     leds[NUM_LEDS - 3] = CRGB::Black;
   }
+
   FastLED.show();
   blynk_delay(4);
 }
@@ -336,6 +350,7 @@ void flow() {
 // Mode Picker
 BLYNK_WRITE(V0) {
   mode = param.asInt();
+  // If mode changes to static, set RGB control to current color
   if (mode == STATIC) {
     Blynk.syncVirtual(V3); // Sync RGB control
   }
@@ -353,6 +368,7 @@ BLYNK_WRITE(V4) {
 
 // RGB Control
 BLYNK_WRITE(V3) {
+  // Only enable RGB control in static mode 
   if (mode == STATIC) {
     r = param[0].asInt();
     g = param[1].asInt();
@@ -366,11 +382,14 @@ BLYNK_CONNECTED() {
   Blynk.syncAll();
 }
 
-// Delay without stopping processes
+// Non-blocking delay
 void blynk_delay(int milli) {
   int end = millis() + milli;
   while (millis() < end) {
+    // Check for incoming blynk commands
     Blynk.run();
+    // Allow ESP to reset timer to prevent resets
+    yield();
   }
 }
 
@@ -396,10 +415,7 @@ double readMic() {
   while (millis() - startMillis < sampleWindow)  {
     sample = analogRead(MIC_PIN);
     blynk_delay(4);    //Need this delay otherwise esp8266 gets locked up.
-    if (mode != MUSIC) {
-      return -1;
-    }
-    yield();
+    if (mode != MUSIC) return -1;
     if (sample < 1024)  {
       if (sample > signalMax) {
         signalMax = sample;  // save just the max levels
@@ -412,6 +428,20 @@ double readMic() {
   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
   double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
   return volts;
+}
+
+void fft_sample(){
+  for (int i = 0; i < SAMPLES; i++) {
+    microseconds = micros();    //Overflows after around 70 minutes!
+
+    vReal[i] = analogRead(0);
+    vImag[i] = 0;
+    Serial.print("vReal: ");
+    Serial.println(vReal[i]);
+
+    while (micros() < (microseconds + sampling_period_us)) {
+    }
+  }
 }
 
 void freq_1_Band(int band, int len){
